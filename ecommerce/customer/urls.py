51b0d2899f8b42b245b747b39d7d191080c8e672
Rubaiyat Toslim <srtoslim@gmail.com>
2023-08-03 13:53:26 +0600

    Delete ecommerce directory

--- ecommerce/customer/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import *
-
-urlpatterns = [
-    path('dashboard/', UserDashboard, name='userDashboard'),
-    path('order-history/', CustomerOrder, name='userOrder'),
-    path('profile/', CustomerInfo, name='userProfile'),
-]
